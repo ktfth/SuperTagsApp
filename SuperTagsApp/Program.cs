78d4664907356f881c0cc34b0937c7db06701a92
Kaique da Silva <kaeyosthaeron@gmail.com>
2022-08-10 16:39:45 -0300

    Project started

---
+++ SuperTagsApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using SuperTags;
+
+namespace SuperTagsApp
+{
+    class SuperTags
+    {
+        static Expansion _expansion = new Expansion();
+        static void Main(string[] args)
+        {
+            if (args.Length == 2)
+            {
+                Console.WriteLine(_expansion.Nested(args[0], args[1]));
+            } 
+            else if (args.Length == 1)
+            {
+                Console.WriteLine(_expansion.Nested(args[0], ""));
+            } 
+            else
+            {
+                Console.WriteLine("SuperTagsApp <pattern> <value>");
+            }
+        }
+    }
+}
